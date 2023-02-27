1dca7e5ee516f17d00979684f40ccadb892c8784
whoismin <99838930+whoismin@users.noreply.github.com>
2023-02-27 16:17:24 -0300

    Add files via upload

---
+++ aula27.02.23/index.php
@@ -0,0 +1,35 @@
+<?php
+
+//Criaçaõ de variavél
+
+$a = 0;
+
+//Sistema de decisão
+
+$b = 10;
+if($b <= 10){
+    echo "O valor da variável b é maior ou igual a 10";
+}else{
+    echo "O valor da variável b é menor que 10";
+}
+
+//Laços de Repetição
+for($i = 0; $i <=10 ; $i++){
+    echo $i;
+
+}
+
+$x = 0;
+while($x <= 10){
+  echo $x;
+  $x++;
+
+}
+
+$y = 0;
+do{
+    echo $y;
+    $y++;
+}while($y <= 10);
+
+?>
---
+++ aula27.02.23/pessoaclasse.php
@@ -0,0 +1,26 @@
+<?php
+
+//Classe
+Public class Pessoa(){
+//Arquivos
+private $nome;
+private $enderço;
+private $bairro;
+private $cep;
+private $cidade;
+private $estado;
+
+//Metodo GET
+public function getNome(){
+return $this->nome
+}
+
+//Metodo SET
+public function stNome($nome){
+$this -> nome = $nome;
+}
+
+
+}
+?>
+
